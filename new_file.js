7d9f409226785735ef296ecdbcff615316e91445
ravikumar <ravipersonalemail@gmail.com>
2024-05-25 16:04:40 +0530

    temp

--- new_file.js
+++ new_file.js
@@ -1,7 +1,5 @@
 let a = {};
-
 Object.defineProperty(a, "name" , {value : 'hell' , writable :false , configurable:true , enumerable:true} );
 console.log( a );
-
 a.name  = 'modified'
 console.log( a.name)
