4301aff3d5ea025768b8e615445d93448f9f7009
ravikumar <ravipersonalemail@gmail.com>
2024-05-25 15:37:08 +0530

    Adding the new typescript file

---
+++ new_file.ts
@@ -0,0 +1,11 @@
+type MyObject = {
+    readonly account_number:"123" ;
+}
+
+
+function accept_account_number(a : MyObject)
+{
+        console.log (a.account_number)
+}
+
+accept_account_number(  {account_number : "123"});
