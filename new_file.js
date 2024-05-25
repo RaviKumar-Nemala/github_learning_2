443b016022f8fe5dd8bc88416b8c01e613945e7d
ravikumar <ravipersonalemail@gmail.com>
2024-05-25 16:24:52 +0530

    hell

--- new_file.js
+++
@@ -1,9 +0,0 @@
-let a = {};
-
-Object.defineProperty(a, "name" , {value : 'hell' , writable :false , configurable:true , enumerable:true} );
-console.log( a );
-
-a.name  = 'modified'
-console.log( a.name)
-
-console.log( 'this is newbranch line')
--- new_file.ts
+++ new_file.ts
@@ -1,11 +1,8 @@
 type MyObject = {
     readonly account_number:"123" ;
 }
-
-
 function accept_account_number(a : MyObject)
 {
         console.log (a.account_number)
 }
-
 accept_account_number(  {account_number : "123"});
