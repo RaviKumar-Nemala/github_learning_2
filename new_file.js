f37bf92b10cd2e30e82dd3649de05cb858e794ec
ravikumar <ravipersonalemail@gmail.com>
2024-05-25 16:28:28 +0530

    hell and heaven

--- new_file.js
+++ new_file.js
@@ -3,4 +3,3 @@ Object.defineProperty(a, "name" , {value : 'hell' , writable :false , configurab
 console.log( a );
 a.name  = 'modified'
 console.log( a.name)
-console.log( 'this is newbranch line')
