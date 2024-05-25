0aa027594f4a8f3b91563927cb0c7c633b953a39
ravikumar <ravipersonalemail@gmail.com>
2024-05-25 15:55:12 +0530

    ADDING THE NEW JS FILE

---
+++ new_file.js
@@ -0,0 +1,7 @@
+let a = {};
+
+Object.defineProperty(a, "name" , {value : 'hell' , writable :false , configurable:true , enumerable:true} );
+console.log( a );
+
+a.name  = 'modified'
+console.log( a.name)
